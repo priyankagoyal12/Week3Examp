97c3de57f6bfc82bfef79f28529e763a11562c26
priyanka priyanka <priyan21@sheridancollege.ca>
2023-05-24 19:18:35 -0400

    Version 2

--- src/gitoperations/GitOperations.java
+++ src/gitoperations/GitOperations.java
@@ -14,7 +14,7 @@ public class GitOperations {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("To learn github Operations");
+        System.out.println("To learn github Operations commit");
         
     }
     
